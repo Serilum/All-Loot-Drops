64075ccced1355a96f7714a90544f014e1bec691
Rick South <rick@natamus.com>
2025-02-12 23:46:52 +0100

    (v3.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/alllootdrops/forge/events/ForgeEntityDroppingEvent.java
+++ Forge/src/main/java/com/natamus/alllootdrops/forge/events/ForgeEntityDroppingEvent.java
@@ -8,12 +8,10 @@ import net.minecraft.world.level.Level;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeEntityDroppingEvent {
 	@SubscribeEvent
-	public void onWorldLoad(LevelEvent.Load e) {
+	public static void onWorldLoad(LevelEvent.Load e) {
 		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
 		if (level == null) {
 			return;
@@ -23,7 +21,7 @@ public class ForgeEntityDroppingEvent {
 	}
 	
 	@SubscribeEvent
-	public void mobItemDrop(LivingDropsEvent e) {
+	public static void mobItemDrop(LivingDropsEvent e) {
 		LivingEntity livingEntity = e.getEntity();
 		EntityDroppingEvent.mobItemDrop(livingEntity.level(), livingEntity, e.getSource());
 	}
