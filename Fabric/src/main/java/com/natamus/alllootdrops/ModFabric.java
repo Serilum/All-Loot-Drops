a33b7420bac72d631c1b203290ace50dad11d56e
Rick South <rick@natamus.com>
2025-07-01 10:34:05 +0200

    All Loot Drops source code for 1.21.7.

---
+++ Fabric/src/main/java/com/natamus/alllootdrops/ModFabric.java
@@ -0,0 +1,45 @@
+package com.natamus.alllootdrops;
+
+import com.natamus.alllootdrops.events.EntityDroppingEvent;
+import com.natamus.alllootdrops.util.Reference;
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.collective.fabric.callbacks.CollectiveEntityEvents;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		ServerWorldEvents.LOAD.register((MinecraftServer server, ServerLevel serverLevel) -> {
+			EntityDroppingEvent.onWorldLoad(serverLevel);
+		});
+
+		CollectiveEntityEvents.ON_ENTITY_IS_DROPPING_LOOT.register((Level level, Entity entity, DamageSource damageSource) -> {
+			EntityDroppingEvent.mobItemDrop(level, entity, damageSource);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
